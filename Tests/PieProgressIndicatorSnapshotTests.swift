2e9fa9df5eeeab6abec34f14c29ace30aa62ea4e
niklaskroe <78739302+niklaskroe@users.noreply.github.com>
2026-07-15 22:04:59 +0200

    fix: perceptual precision to account for anti aliasing diff

--- Tests/PieProgressIndicatorSnapshotTests.swift
+++ Tests/PieProgressIndicatorSnapshotTests.swift
@@ -17,11 +17,11 @@ final class PieProgressIndicatorSnapshotTests: XCTestCase {
         #if os(macOS)
         let quarterHost = NSHostingView(rootView: quarterView)
         quarterHost.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        assertSnapshot(of: quarterHost, as: .image(precision: 0.99), named: "mac_25_Percent")
+        assertSnapshot(of: quarterHost, as: .image(precision: 0.99, perceptualPrecision: 0.98), named: "mac_25_Percent")
         #else
         assertSnapshot(
             of: quarterView,
-            as: .image(precision: 0.99, layout: .fixed(width: 100, height: 100)),
+            as: .image(precision: 0.99, perceptualPrecision: 0.98, layout: .fixed(width: 100, height: 100)),
             named: "25_Percent"
         )
         #endif
@@ -33,11 +33,11 @@ final class PieProgressIndicatorSnapshotTests: XCTestCase {
         #if os(macOS)
         let fullHost = NSHostingView(rootView: fullView)
         fullHost.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        assertSnapshot(of: fullHost, as: .image(precision: 0.99), named: "mac_100_Percent")
+        assertSnapshot(of: fullHost, as: .image(precision: 0.99, perceptualPrecision: 0.98), named: "mac_100_Percent")
         #else
         assertSnapshot(
             of: fullView,
-            as: .image(precision: 0.99, layout: .fixed(width: 100, height: 100)),
+            as: .image(precision: 0.99, perceptualPrecision: 0.98, layout: .fixed(width: 100, height: 100)),
             named: "100_Percent"
         )
         #endif
